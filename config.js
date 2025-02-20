010b69f88f6f47e5970a91015e43d5ee6e600f7a
akhileshthite <akhithite@gmail.com>
2025-02-20 12:55:45 -0800

    feat: add USDT contract addresses for OP and ARB

--- config.js
+++ config.js
@@ -4,20 +4,29 @@ const donationConfigs = {
     tokenContract: null,
     decimals: null,
     isNative: true,
-    explorer: 'https://sepolia.etherscan.io/tx/'
+    explorer: 'https://etherscan.io/tx/'
   },
   USDT: {
     donationWallet: '0xBFCe3465fBEBC928c922362CBCdC32bEF8BCbE41',
-    tokenContract: '0x4200000000000000000000000000000000000042',
+    tokenContract: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    decimals: 6,
+    isNative: false,
+    explorer: 'https://etherscan.io/tx/'
+  },
+  USDT_OP: {
+    donationWallet: '0xBFCe3465fBEBC928c922362CBCdC32bEF8BCbE41',
+    tokenContract: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58',
     decimals: 6,
     isNative: false,
     explorer: 'https://optimistic.etherscan.io/tx/'
   },
-  ARB: {
+  USDT_ARB: {
     donationWallet: '0xBFCe3465fBEBC928c922362CBCdC32bEF8BCbE41',
-    tokenContract: '0x912ce59144191c1204e64559fe8253a0e49e6548',
+    tokenContract: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
     decimals: 6,
     isNative: false,
     explorer: 'https://arbiscan.io/tx/'
   }
 }
+
+
