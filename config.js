03c13dbb13da23f04640bee327f432af2264a145
akhileshthite <akhithite@gmail.com>
2025-02-20 12:12:01 -0800

    feat: add DP wallet address and token contract adresses

--- config.js
+++ config.js
@@ -1,16 +1,23 @@
 const donationConfigs = {
   ETH: {
-    donationWallet: '0xETHWalletAddress',
+    donationWallet: '0xBFCe3465fBEBC928c922362CBCdC32bEF8BCbE41',
     tokenContract: null,
     decimals: null,
     isNative: true,
     explorer: 'https://sepolia.etherscan.io/tx/'
   },
   USDT: {
-    donationWallet: '0xETHWalletAddress',
-    tokenContract: '0xOPContractAddress',
+    donationWallet: '0xBFCe3465fBEBC928c922362CBCdC32bEF8BCbE41',
+    tokenContract: '0x4200000000000000000000000000000000000042',
     decimals: 6,
     isNative: false,
     explorer: 'https://optimistic.etherscan.io/tx/'
+  },
+  ARB: {
+    donationWallet: '0xBFCe3465fBEBC928c922362CBCdC32bEF8BCbE41',
+    tokenContract: '0x912ce59144191c1204e64559fe8253a0e49e6548',
+    decimals: 6,
+    isNative: false,
+    explorer: 'https://arbiscan.io/tx/'
   }
 }
